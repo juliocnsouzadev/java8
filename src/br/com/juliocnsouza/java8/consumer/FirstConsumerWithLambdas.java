6a1ef0095d3abe8f7a954ebb8b81a2a4622138ba
Guilherme <Guilherme@ThinkPadT430>
2015-08-29 21:10:27 -0300

    agora usando lambdas

---
+++ src/br/com/juliocnsouza/java8/consumer/FirstConsumerWithLambdas.java
@@ -0,0 +1,34 @@
+package br.com.juliocnsouza.java8.consumer;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ *
+ * @author juliocnsouza
+ */
+public class FirstConsumerWithLambdas {
+
+    public static void main( String[] args ) {
+
+        List<String> palavras = Arrays.asList( "julio" , "cesar" , "nunes" ,
+                                               "de" , "souza" );
+
+        palavras.sort(( String s1 , String s2 ) -> {
+            if ( s1.length() < s2.length() ) {
+                return -1;
+            }
+            if ( s1.length() > s2.length() ) {
+                return 1;
+            }
+            return 0;
+        });
+
+        palavras.forEach( ( String palavra ) -> {
+            System.out.println( palavra );
+        } );
+
+    }
+
+}
