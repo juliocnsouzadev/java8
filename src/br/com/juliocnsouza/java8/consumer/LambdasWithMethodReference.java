abae1f127a96905e5f22a1f026f8cac7f0a48902
Julio CN Souza <juliocnsouzadev@gmail.com>
2015-08-30 17:50:00 -0300

    começando com method reference

---
+++ src/br/com/juliocnsouza/java8/consumer/LambdasWithMethodReference.java
@@ -0,0 +1,28 @@
+package br.com.juliocnsouza.java8.consumer;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ *
+ * @author juliocnsouza
+ */
+public class LambdasWithMethodReference {
+
+    public static void main( String[] args ) {
+
+        List<String> palavras = Arrays.asList( "julio" , "cesar" , "nunes" ,
+                                               "de" , "souza" );
+
+        palavras.sort( Comparator.comparing( s -> s.length() ) );
+        palavras.sort( Comparator.comparing( String :: length ) );
+
+        palavras.sort( Comparator.comparing( s -> s.hashCode() ) );
+        palavras.sort( Comparator.comparing( String :: hashCode ) );
+
+        palavras.forEach( System.out :: println );
+
+    }
+
+}
